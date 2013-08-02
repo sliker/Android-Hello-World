00540d278a7b856455e38459859378b76dcc3b7a
David Velásquez <zero.blend@gmail.com>
2013-08-02 14:50:16 -0500

    initial commit

---
+++ Hello_World/build/source/buildConfig/debug/com/davidvu/hello_world/BuildConfig.java
@@ -0,0 +1,7 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.davidvu.hello_world;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+
+}
---
+++ Hello_World/src/main/java/com/davidvu/hello_world/WorldGen.java
@@ -0,0 +1,8 @@
+package com.davidvu.hello_world;
+
+/**
+ * Created by david.velasquez on 8/2/13.
+ */
+public class WorldGen {
+    /** here we go **/
+}
