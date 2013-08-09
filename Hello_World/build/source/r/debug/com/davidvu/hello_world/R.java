d62542aa09e056e69150d82d4e9c3884377e171a
David Velásquez <zero.blend@gmail.com>
2013-08-09 15:30:22 -0500

    Xml strings

--- Hello_World/build/source/r/debug/com/davidvu/hello_world/R.java
+++ Hello_World/build/source/r/debug/com/davidvu/hello_world/R.java
@@ -21,7 +21,23 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080010;
+        public static final int dataView1=0x7f080008;
+        public static final int dataView2=0x7f080009;
+        public static final int dataView3=0x7f08000a;
+        public static final int dataView4=0x7f08000b;
+        public static final int dataView5=0x7f08000c;
+        public static final int dataView6=0x7f08000d;
+        public static final int dataView7=0x7f08000e;
+        public static final int dataView8=0x7f08000f;
+        public static final int textView1=0x7f080000;
+        public static final int textView2=0x7f080001;
+        public static final int textView3=0x7f080002;
+        public static final int textView4=0x7f080003;
+        public static final int textView5=0x7f080004;
+        public static final int textView6=0x7f080005;
+        public static final int textView7=0x7f080006;
+        public static final int textView8=0x7f080007;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -35,6 +51,14 @@ public final class R {
         public static final int action_settings=0x7f050000;
         public static final int app_name=0x7f050001;
         public static final int hello_world=0x7f050002;
+        public static final int planet_bases_label=0x7f050003;
+        public static final int planet_colonies_label=0x7f050004;
+        public static final int planet_forcefield_label=0x7f050005;
+        public static final int planet_gravity_label=0x7f050006;
+        public static final int planet_mass_label=0x7f050007;
+        public static final int planet_military_label=0x7f050008;
+        public static final int planet_name_label=0x7f050009;
+        public static final int planet_population_label=0x7f05000a;
     }
     public static final class style {
         /**  From: file:/Users/david.velasquez/AndroidStudioProjects/Hello_WorldProject/Hello_World/src/main/res/values/styles.xml 
--- Hello_World/src/main/java/com/davidvu/hello_world/MainActivity.java
+++ Hello_World/src/main/java/com/davidvu/hello_world/MainActivity.java
@@ -3,19 +3,28 @@ package com.davidvu.hello_world;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+    WorldGen earth = new WorldGen("Earth", 5973, 9.78);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        WorldGen earth = new WorldGen("Earth", 5973, 9.78);
+        setStartUpWorldValues();
+        setStartUpScreenText();
+    }
+    protected void setStartUpWorldValues() {
         earth.setPlanetColonies(1);
         earth.setPlanetMilitary(1);
         earth.setColonyImmigration(1000);
         earth.setBaseProtection(100);
         earth.turnForceFieldOn();
+    }
+    private void setStartUpScreenText() {
+        TextView planetNameValue = (TextView)findViewById(R.id.dataView1);
+        planetNameValue.setText(earth.planetName);
     }
 
 
