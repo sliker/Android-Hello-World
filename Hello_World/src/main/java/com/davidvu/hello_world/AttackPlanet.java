2a65dc23997dc2373ce65648947495936dc299cd
David Velásquez <zero.blend@gmail.com>
2013-08-14 13:39:39 -0500

    All activities

---
+++ Hello_World/src/main/java/com/davidvu/hello_world/AttackPlanet.java
@@ -0,0 +1,15 @@
+package com.davidvu.hello_world;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by david.velasquez on 8/14/13.
+ */
+public class AttackPlanet extends Activity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_attack);
+    }
+}
--- Hello_World/src/main/java/com/davidvu/hello_world/TravelPlanetActivity.java
+++ Hello_World/src/main/java/com/davidvu/hello_world/TravelPlanetActivity.java
@@ -4,19 +4,19 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
-public class NewPlanet extends Activity {
+public class TravelPlanetActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add);
+        setContentView(R.layout.activity_travel);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.new_planet, menu);
+        getMenuInflater().inflate(R.menu.travel_planet, menu);
         return true;
     }
     
