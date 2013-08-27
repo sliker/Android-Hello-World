4b5d094e250a758eb7c37447d4fffd9b947f4184
David Velásquez <zero.blend@gmail.com>
2013-08-27 13:31:42 -0500

    Attack on planet

--- Hello_World/src/main/java/com/davidvu/hello_world/AttackPlanetActivity.java
+++ Hello_World/src/main/java/com/davidvu/hello_world/AttackPlanetActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 /**
  * Created by david.velasquez on 8/14/13.
@@ -14,6 +15,35 @@ public class AttackPlanetActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_attack);
+
+        ImageButton bombButton = (ImageButton) findViewById(R.id.bombButton);
+        bombButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(AttackPlanetActivity.this, "Bomb away!", Toast.LENGTH_SHORT).show();
+            }
+        });
+        ImageButton invadeButton = (ImageButton) findViewById(R.id.invadeButton);
+        invadeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(AttackPlanetActivity.this, "Troops send", Toast.LENGTH_SHORT).show();
+            }
+        });
+        ImageButton infectButton = (ImageButton) findViewById(R.id.infectButton);
+        infectButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(AttackPlanetActivity.this, "Virus Spread", Toast.LENGTH_SHORT).show();
+            }
+        });
+        ImageButton laserButton = (ImageButton) findViewById(R.id.laserButton);
+        laserButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(AttackPlanetActivity.this, "Laser Fired!", Toast.LENGTH_SHORT).show();
+            }
+        });
         ImageButton exitButton = (ImageButton) findViewById(R.id.exitButton);
         exitButton.setOnClickListener(new View.OnClickListener() {
             @Override
